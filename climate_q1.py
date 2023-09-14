7d49ef9689e068a25a05c8dd5e160015499bbfec
lordfaaquad <127821143+lordfaaquad@users.noreply.github.com>
2023-09-14 20:59:12 +1000

    Added code to fetch data from the database

--- climate_q1.py
+++ climate_q1.py
@@ -15,4 +15,19 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+
+import sqlite3
+
+conn=sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+dates=[]
+temperatures =[]
+
+cursor.execute('SELECT date, temperature FROM climate_data')
+for row in curaor.fetchall():
+    dates.append(row[0])
+    temperatures.append(row[1])
+
+conn.close()
