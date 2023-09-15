453e7d0b92c20204b8bab93d4fd6615a19112d87
lordfaaquad <127821143+lordfaaquad@users.noreply.github.com>
2023-09-15 19:23:39 +1000

    Added code to load data from CSV and display in a figure

--- climate_q2.py
+++ climate_q2.py
@@ -18,3 +18,16 @@ plt.xlabel("Year (decade)")
 plt.show() 
 plt.savefig("co2_temp_2.png") 
 
+data = pd.read_csv('data.csv')
+
+dates = data['Date']
+values = data['Value']
+plt.figure(figsize=(10, 6))
+plt.plot(dates, values, marker='o', linestyle='-')
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.title('Test Data from CSV File')
+plt.grid(True)
+plt.xticks(rotation=45)
+
+plt.show()
